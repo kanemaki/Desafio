9fc9a6b7f76b16f56cc915e22e66525ac09ff642
Alexandre Florentino <kanemaki@gmail.com>
2021-08-15 00:26:33 +0100

    feat: add novoUsuario knex and Graphql.

--- resolvers/Mutation/usuario.js
+++ resolvers/Mutation/usuario.js
@@ -1,8 +1,16 @@
 const db = require('../../config/db')
+const { perfil: obterPerfil } = require('../Query/perfil')
 
 module.exports = {
     async novoUsuario(_, { dados }) {
-        // Implementar
+        try{
+            const [ id ] = await db('perfis')
+                .insert({ ...dados })
+            return db('perfis')
+                .where({ id }).first()    
+        } catch(e) {
+            throw new Error(e.sqlMessage)
+        }
     },
     async excluirUsuario(_, { filtro }) {
         // Implementar
